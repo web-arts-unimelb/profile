2a2f7a8f4b6f7c6a56b4f7b7d5db2ce1351c25ef
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-07-09 15:37:06 +1000

    add html.tpl.php

---
+++ themes/custom/bootstrap_unimelb/html.tpl.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!-- HTML5 element support for IE6-8 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<!-- Google Tag Manager -->
+
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-7JB9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-7JB9');</script>
+<!-- End Google Tag Manager -->
+
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
