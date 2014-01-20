86c62e43daf0dfe7e5b403e6288ba502220dd381
arts-unimelb <arts-webmaster@unimelb.edu.au>
2014-01-20 22:59:58 +1100

    Commit an empty script

--- themes/custom/bootstrap_unimelb/js/bootstrap_unimelb.js
+++ themes/custom/bootstrap_unimelb/js/bootstrap_unimelb.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.bootstrap_unimelb = {
     attach: function(context, settings) {
 			/* Start welcome.arts.unimelb.edu.au */
-			$('.nav-collapse').height('auto');
 			/* End welcome.arts.unimelb.edu.au */
 		}
 	}
