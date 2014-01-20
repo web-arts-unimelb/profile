f7339193f893317b13168460510c15f1e3b34c8e
arts-unimelb <arts-webmaster@unimelb.edu.au>
2014-01-20 16:35:27 +1100

    add javascript

---
+++ themes/custom/bootstrap_unimelb/js/bootstrap_unimelb.js
@@ -0,0 +1,10 @@
+(function ($) {
+  Drupal.behaviors.bootstrap_unimelb = {
+    attach: function(context, settings) {
+			/* Start welcome.arts.unimelb.edu.au */
+			$('.nav-collapse').height('auto');
+			/* End welcome.arts.unimelb.edu.au */
+		}
+	}
+})(jQuery);
+
